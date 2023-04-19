fda42f1fd07c34dd7988e0b4e32efa137454a369
Dadykad <Dadykad@gmail.com>
2023-04-19 18:47:03 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let מהירות_סיבוב = 0
+let הפעלה = 0
 let גילגול = 0
 let עילרוד = 0
 input.onButtonPressed(Button.A, function () {
@@ -8,12 +9,24 @@ input.onButtonPressed(Button.A, function () {
         מהירות_סיבוב += 1
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    if (הפעלה == 1) {
+        הפעלה = 0
+    } else {
+        הפעלה = 1
+    }
+    מהירות_סיבוב = 0
+})
 input.onButtonPressed(Button.B, function () {
     if (מהירות_סיבוב <= 40) {
         מהירות_סיבוב += -5
     } else {
         מהירות_סיבוב += -1
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    מהירות_סיבוב = 0
+    הפעלה = 0
 })
 basic.forever(function () {
     גילגול = input.rotation(Rotation.Roll)
