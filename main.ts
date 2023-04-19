48ad0e2ee2d454d1bba97e97ca09552a5e13437f
Dadykad <Dadykad@gmail.com>
2023-04-19 18:24:35 +0300

    משתני מהירות סיבובת עלרוד וגלגול

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 let מהירות_סיבוב = 0
+let גילגול = 0
+let עילרוד = 0
 input.onButtonPressed(Button.A, function () {
     if (מהירות_סיבוב <= 40) {
         מהירות_סיבוב += 5
@@ -7,5 +9,13 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    מהירות_סיבוב += -5
+    if (מהירות_סיבוב <= 40) {
+        מהירות_סיבוב += -5
+    } else {
+        מהירות_סיבוב += -1
+    }
+})
+basic.forever(function () {
+    גילגול = input.rotation(Rotation.Roll)
+    עילרוד = input.rotation(Rotation.Pitch)
 })
